ee3dc08d4f7c7265cbf1b525a9f2fae23ddd3e04
Muhammad Abba <muhammadabba9741@gmail.com>
2025-01-20 21:50:09 +0100

    choboi

--- choboishop/orders/admin.py
+++ choboishop/orders/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def order_payment(obj):
+    url = obj.get_stripe_url()
+
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+
+    return ''
+
+
+order_payment.short_description = 'Stripe payment'
 
 
 @admin.register(Order)
@@ -19,6 +33,7 @@ class OrderAdmin(admin.ModelAdmin):
         'city',
         'paid',
         'created',
+        order_payment,
         'updated'
     ]
     list_filter = ['paid', 'created', 'updated']
---
+++ choboishop/payment/webhooks.py
@@ -0,0 +1,52 @@
+import stripe
+import logging
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from orders.models import Order
+
+logger = logging.getLogger(__name__)
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    """
+    Webhook handler for Stripe events.
+
+    This function handles incoming webhook events from Stripe. It verifies the
+    signature of the event, retrieves the event data, and then performs actions
+    based on the event type.
+
+    For example, if the event is a 'payment_intent.succeeded' event, this function
+    marks the corresponding order as paid.
+    """
+
+    payload = request.body
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE')
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        logger.error(f"Invalid payload: {e}")
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        logger.error(f"Invalid signature: {e}")
+        return HttpResponse(status=400)
+
+    # Handle the event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        if session['mode'] == 'payment' and session['payment_status'] == 'paid':
+            try:
+                order = Order.objects.get(id=session['client_reference_id'])
+                order.paid = True
+                order.stripe_id = session.payment_intent
+                order.save()
+            except Order.DoesNotExist:
+                logger.error(f"Order with id {session['client_reference_id']} does not exist.")
+                return HttpResponse(status=404)
+
+    return HttpResponse(status=200)
